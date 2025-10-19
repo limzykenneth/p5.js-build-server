1b201be1765a4958e4dda25b8330906ff29dd22f
limzykenneth <limzy.kenneth@gmail.com>
2025-10-19 16:29:46 +0100

    Implement more caching

--- workspaces/worker/src/index.ts
+++ workspaces/worker/src/index.ts
@@ -1,6 +1,9 @@
 import { Hono } from 'hono';
+import { cors } from 'hono/cors';
 
 const app = new Hono<{Bindings: Env}>();
+
+app.use(cors());
 
 app.get('/:path{.+}', async (c) => {
 	let path = c.req.param("path");
@@ -11,32 +14,76 @@ app.get('/:path{.+}', async (c) => {
 		requestedModules.sort();
 		path += `(${requestedModules.join("__")})`;
 	}
-	const data = await c.env.BUILDS.get(path);
 
-	if(data){
-		c.header("Content-Type", "text/javascript; charset=utf-8");
-		return c.body(await data.text());
-	}else{
-		let res: Response;
-		if(c.env.DEV === "true"){
-			const originalUrl = new URL(c.req.raw.url);
-			const targetUrl = new URL(c.env.BUILD_SERVER);
-			originalUrl.host = targetUrl.host;
-			const req = new Request(originalUrl, c.req.raw);
-			res = await fetch(req);
+	const cache = caches.default;
+	let response = await cache.match(c.req.url);
+	let contentHash: ArrayBuffer;
+
+	if(!response){
+		// Data not in worker cache
+		console.log("Missed cache");
+		const data = await c.env.BUILDS.get(path);
+
+		if(data){
+			// Data cached in R2
+			contentHash = data.checksums.sha256;
+			c.header("Content-Type", "text/javascript; charset=utf-8");
+			response = c.body(await data.text());
 		}else{
-			res = await fetch(c.req.raw);
+			// Data not cached anywhere, generate from origin server
+			let res: Response;
+			if(c.env.DEV === "true"){
+				const originalUrl = new URL(c.req.raw.url);
+				const targetUrl = new URL(c.env.BUILD_SERVER);
+				originalUrl.host = targetUrl.host;
+				const req = new Request(originalUrl, c.req.raw);
+				res = await fetch(req);
+			}else{
+				res = await fetch(c.req.raw);
+			}
+
+			// Return value from origin server
+			if(res.ok){
+				const body = await res.text();
+				contentHash = await digestMessage(body);
+				c.executionCtx.waitUntil(c.env.BUILDS.put(path, body, {
+					sha256: contentHash
+				}));
+				c.header("Content-Type", "text/javascript; charset=utf-8");
+				response = c.body(body);
+			}else{
+				// Error retrieving data, return right away
+				return res;
+			}
 		}
 
-		if(res.ok){
-			const body = await res.text();
-			await c.env.BUILDS.put(path, body);
-			c.header("Content-Type", "text/javascript");
-			return c.body(body);
-		}else{
-			return res;
-		}
+		c.executionCtx.waitUntil(cache.put(c.req.url, response.clone()));
 	}
+
+	c.header("Content-Type", "text/javascript; charset=utf-8");
+	c.header("etag", bufferToBase64(contentHash));
+	c.header("Cache-Control", "public, max-age=31536000");
+
+	return response;
 });
+
+async function digestMessage(message: string) {
+  const msgUint8 = new TextEncoder().encode(message); // encode as (utf-8) Uint8Array
+  const hashBuffer = await crypto.subtle.digest("SHA-256", msgUint8); // hash the message
+  return hashBuffer;
+  // const hashArray = Array.from(new Uint8Array(hashBuffer)); // convert buffer to byte array
+  // const hashHex = hashArray
+  //   .map((b) => String.fromCharCode(b))
+  //   .join(""); // convert bytes to hex string
+  // return btoa(hashHex);
+}
+
+function bufferToBase64(buf: ArrayBuffer){
+	const hashArray = Array.from(new Uint8Array(buf)); // convert buffer to byte array
+  const hashHex = hashArray
+    .map((b) => String.fromCharCode(b))
+    .join(""); // convert bytes to hex string
+  return btoa(hashHex);
+}
 
 export default app;
