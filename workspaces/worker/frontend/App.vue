aeb589907bd5b04bef9ec783fb4e4411a3e3e88f
limzykenneth <limzy.kenneth@gmail.com>
2025-11-03 13:14:33 +0000

    Frontend styling

--- workspaces/worker/frontend/App.vue
+++ workspaces/worker/frontend/App.vue
@@ -17,7 +17,7 @@ const moduleString = computed(() => {
     if (isSelected) acc.push(name);
     return acc;
   }, []);
-  return `?modules=${selectedModules.join(",")}`;
+  return `?modules=${selectedModules.join(',')}`;
 });
 
 const toggleModule = (name: string) => {
@@ -34,59 +34,107 @@ const bundleURL = computed(() => {
       return `${window.location.origin}/${version.value}/${bundle.value}`;
     }
   }
-  return ""
+  return ''
 });
 </script>
 
 <template>
-	<div>
-	    <span>Please give me URL for</span>
+  <div class="main-container">
+  	<div class="selector-container">
+      <span>Please give me URL for</span>
 
-		<select v-model="bundle">
-           	<option value="p5.js">p5.js</option>
-            <option value="p5.min.js">p5.min.js</option>
-            <option value="p5.mod.js">p5.js modules</option>
-            <option value="p5.custom.js">p5.js custom build</option>
-	    </select>
+  		<select v-model="bundle">
+       	<option value="p5.js">p5.js</option>
+        <option value="p5.min.js">p5.min.js</option>
+        <option value="p5.mod.js">p5.js modules</option>
+        <option value="p5.custom.js">p5.js custom build</option>
+      </select>
 
-		<span>at version</span>
+  		<span>at version</span>
 
-	    <select v-model="version">
-			<option>2.0.5</option>
-			<option>2.0.4</option>
-			<option>2.0.3</option>
-			<option>2.0.2</option>
-			<option>2.0.1</option>
-		</select>
+      <select v-model="version">
+  			<option>2.0.5</option>
+  			<option>2.0.4</option>
+  			<option>2.0.3</option>
+  			<option>2.0.2</option>
+  			<option>2.0.1</option>
+  		</select>
 
-		<span v-if="bundle === 'p5.mod.js'">
-		    for module
-			<select v-if="bundle === 'p5.mod.js'" v-model="singleModule">
-			    <option v-for="(isSelected, name) in p5Modules">{{ name }}</option>
-            </select>
-		</span>
+  		<span v-if="bundle === 'p5.mod.js'">
+  	    for module
+  			<select v-if="bundle === 'p5.mod.js'" v-model="singleModule">
+  		    <option v-for="(isSelected, name) in p5Modules">{{ name }}</option>
+        </select>
+  		</span>
 
-        <span v-if="bundle === 'p5.custom.js'">
-            with modules
-            <button
-                v-for="(isSelected, name) in p5Modules"
-                class="custom-module-button"
-                :class="isSelected ? 'selected' : ''"
-                :disabled="['core'].includes(name)"
-                @click="toggleModule(name)"
-            >{{ name }}</button>
-        </span>
-	</div>
+      <span v-if="bundle === 'p5.custom.js'">
+        with modules
+        <button
+          v-for="(isSelected, name) in p5Modules"
+          class="custom-module-button"
+          :class="isSelected ? 'selected' : ''"
+          :disabled="['core'].includes(name)"
+          @click="toggleModule(name)"
+        >{{ name }}</button>
+      </span>
+  	</div>
 
-	<div>
-	    {{ bundleURL }}
-	</div>
+  	<div class="url-container">
+      {{ bundleURL }}
+  	</div>
+  </div>
 </template>
 
 <style scoped>
-.custom-module-button {
-    &.selected{
-        background: green;
+@import "./stylesheets/variables.css";
+
+.main-container{
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  max-width: 900px;
+
+  .selector-container{
+    padding: 0.5rem;
+
+    select, button{
+      font-size: inherit;
+      font-family: inherit;
     }
+
+    select{
+      appearance: none;
+      background-color: transparent;
+      border: 5px solid var(--bg-magenta-70);
+      border-radius: 0.25em;
+      padding: 0 0.5em;
+      margin: 0;
+      cursor: pointer;
+      line-height: inherit;
+    }
+
+    .custom-module-button {
+      cursor: pointer;
+      background-color: transparent;
+      border: 5px solid var(--bg-magenta-70);
+      border-radius: 0.25em;
+      margin: 0 0.1em;
+      color: black;
+
+      &.selected{
+        background: var(--bg-magenta-20);
+      }
+    }
+  }
+
+  .url-container{
+    min-height: 3.5rem;
+    border: 5px solid var(--bg-magenta-70);
+    border-radius: 0.25em;
+    padding: 0.5rem 1rem;
+    font-family: var(--font-serif);
+    font-size: 0.75em;
+  }
 }
 </style>
